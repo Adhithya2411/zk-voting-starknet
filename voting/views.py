8a878f0249fd85a522e32df08fb762913e09f202
Adhithya2411 <snadhithya2005@gmail.com>
2026-03-28 10:03:33 +0530

    feat(web3): implement dynamic MetaMask connection and real-time payload generation

--- voting/views.py
+++ voting/views.py
@@ -1,8 +1,7 @@
 import sys
 import os
 from django.http import JsonResponse
-from django.shortcuts import render
-
+from django.shortcuts import render 
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from offchain_prover.merkle_builder import PoseidonMerkleTree, NullifierGenerator, CalldataFormatter
@@ -11,32 +10,29 @@ def index(request):
     """Renders the main frontend voting interface."""
     return render(request, 'voting/index.html')
 
-
 def generate_proof(request):
     try:
-        # TODO: These will eventually come from the database/frontend payload
+        target_wallet_hex = request.GET.get('wallet')
+        if not target_wallet_hex:
+            raise ValueError("No wallet address provided by frontend")
+
+        target_voter = int(target_wallet_hex, 16)
         ELECTION_ID = 1
+
         dummy_registered_voters = [
             int("0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7", 16),
             int("0x03d22ce470c14b19642d51fb5dfd553b53cb1912f32777b7cb27a659ccab2136", 16),
-            int("0x01a34382103f56ce43764b8cb6e0817c76b97da05ea7e3f89073c66f57007e60", 16)
+            target_voter
         ]
 
-        target_wallet_hex = "0x03d22ce470c14b19642d51fb5dfd553b53cb1912f32777b7cb27a659ccab2136"
-        target_voter = int(target_wallet_hex, 16)
-
-        # 1. Initialize Tree
         tree = PoseidonMerkleTree(dummy_registered_voters)
         merkle_root = hex(tree.get_root())
 
-        # 2. Get Proof
         proof_data = tree.get_proof(target_voter)
         hex_proof = [hex(p) for p in proof_data]
 
-        # 3. Generate Nullifier
         nullifier = hex(NullifierGenerator.generate_nullifier(target_voter, ELECTION_ID))
 
-        # 4. Format for Cairo
         calldata = CalldataFormatter.format_vote_payload(int(nullifier, 16), proof_data)
         hex_calldata = [hex(x) for x in calldata]
 
